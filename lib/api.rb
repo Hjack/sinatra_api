e3e276c414f117f60c92b91b24c512131d47972e
hakimu jackson <hakimuj@gmail.com>
2016-07-07 23:18:21 -0700

    Added JSON gem to parse data

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,13 @@
+require 'json'
 require 'net/http'
 require './key'
 
 uri = URI("https://api.nasa.gov/neo/rest/v1/feed?start_date=2016-07-01&end_date=2016-07-08&api_key=#{api_key}")
 data = Net::HTTP.get(uri)
+parsed_data = JSON.parse(data)
 
-puts data
+# puts parsed_data
+# puts parsed_data["element_count"]
+puts parsed_data["near_earth_objects"]["2016-07-03"][0]["name"]
+puts parsed_data["near_earth_objects"]["2016-07-03"][0]["close_approach_data"][0]["relative_velocity"]
+puts parsed_data["near_earth_objects"]["2016-07-03"][0]["estimated_diameter"]
