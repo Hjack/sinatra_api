bb3981cb57f7add22b51bffe5f9663ab36d93c52
hakimu jackson <hakimuj@gmail.com>
2016-07-12 23:35:30 -0700

    Created method that pulls 'near_earth_objects'

--- lib/test_parser.rb
+++ lib/test_parser.rb
@@ -13,10 +13,20 @@ def api_parser
   parsed_data = JSON.parse(data)
 end
 
-api_parser
+# parsed_data = api_parser
 
 # The stuff in the testing_logs.logs file is from foo["near_earth_objects"]
 
+# I believe this method returns all "close_approach_data" I need to see why puts returns more arrays than what's shows when I puts test_parser.count
+def test_parser
+	api_parser["near_earth_objects"].map do |key,value|
+		value.map do |array|
+		  array["close_approach_data"]
+		end
+	end
+end
+
+puts test_parser
 # puts parsed_data.class
 # puts parsed_data["near_earth_objects"]["2016-07-03"][0]["name"]
 # puts parsed_data["near_earth_objects"]["2016-07-03"][0]["close_approach_data"][0]["relative_velocity"]
